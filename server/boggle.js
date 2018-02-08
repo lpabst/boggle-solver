5b170ae3c120bcbe36271ade9f71fbc9c624d78b
Loren Pabst <loren.pabst@ALLEGIANCE.LOCAL>
2018-02-08 15:03:29 -0700

    updated dictionary setup and added some test info

--- server/boggle.js
+++ server/boggle.js
@@ -4,77 +4,99 @@ const wordList = require('./dictionary.js');
 const list = wordList.list;
 
 var dictionary = {
-  length9: {},
-  length12: {},
+  length5: {},
+  length8: {},
+  length11: {},
   length16: {},
 };
 
-// Creates a more efficient dictionary
-for (var i in list){
+// Creates a more efficient dictionary, creates subDictionaries based on length, then again based on the first letters of each word
+for (let i = 0; i < list.length; i++){
   let word = list[i];
-  let letters1To3 = word.substring(0,3);
-  let letters4To6 = word.substring(3,6);
-  let letters7To9 = word.substring(6,9);
+  let letters1To2 = word.substring(0,2);
+  let letters3To4 = word.substring(2,4);
+  let letters5To6 = word.substring(4,6);
+  let letters7To8 = word.substring(6,8);
 
-  if (word.length <= 9){
-    if (!dictionary.length9[letters1To3]){
-      dictionary.length9[letters1To3] = [];
+  if (word.length <= 5){
+    if (!dictionary.length5[letters1To2]){
+      dictionary.length5[letters1To2] = [];
     }
-    dictionary.length9[letters1To3].push(word);
-  }else if (word.length <= 12){
-    if (!dictionary.length12[letters1To3]){
-      dictionary.length12[letters1To3] = {};
+    dictionary.length5[letters1To2].push(word);
+  }else if (word.length <= 8){
+    if (!dictionary.length8[letters1To2]){
+      dictionary.length8[letters1To2] = {};
     }
-    if (!dictionary.length12[letters1To3][letters4To6]){
-      dictionary.length12[letters1To3][letters4To6] = [];
+    if (!dictionary.length8[letters1To2][letters3To4]){
+      dictionary.length8[letters1To2][letters3To4] = [];
     }
-    dictionary.length12[letters1To3][letters4To6].push(word);
+    dictionary.length8[letters1To2][letters3To4].push(word);
+  }else if (word.length <= 11){
+    if (!dictionary.length11[letters1To2]){
+      dictionary.length11[letters1To2] = {};
+    }
+    if (!dictionary.length11[letters1To2][letters3To4]){
+      dictionary.length11[letters1To2][letters3To4] = {};
+    }
+    if (!dictionary.length11[letters1To2][letters3To4][letters5To6]){
+      dictionary.length11[letters1To2][letters3To4][letters5To6] = [];
+    }
+    dictionary.length11[letters1To2][letters3To4][letters5To6].push(word);
   }else if (word.length <= 16){
-    if (!dictionary.length16[letters1To3]){
-      dictionary.length16[letters1To3] = {};
+    if (!dictionary.length16[letters1To2]){
+      dictionary.length16[letters1To2] = {};
     }
-    if (!dictionary.length16[letters1To3][letters4To6]){
-      dictionary.length16[letters1To3][letters4To6] = {};
+    if (!dictionary.length16[letters1To2][letters3To4]){
+      dictionary.length16[letters1To2][letters3To4] = {};
     }
-    if (!dictionary.length16[letters1To3][letters4To6][letters7To9]){
-      dictionary.length16[letters1To3][letters4To6][letters7To9] = [];
+    if (!dictionary.length16[letters1To2][letters3To4][letters5To6]){
+      dictionary.length16[letters1To2][letters3To4][letters5To6] = {};
     }
-    dictionary.length16[letters1To3][letters4To6][letters7To9].push(word);
+    if (!dictionary.length16[letters1To2][letters3To4][letters5To6][letters7To8]){
+      dictionary.length16[letters1To2][letters3To4][letters5To6][letters7To8] = [];
+    }
+    dictionary.length16[letters1To2][letters3To4][letters5To6][letters7To8].push(word);
   }
   
 }
 
 let words = [];
 
-let combinationCount = 0;
 let wordCheckCount = 0;
 let debugCount = 0;
 
+// Looks in our dictionary to see if a string of letters is contained in there somewhere
 function isWord(str){
-  // wordCheckCount++;
-  // combinationCount++;
+  wordCheckCount++;
   
-  let letters1To3 = str.substring(0,3);
-  let letters4To6 = str.substring(3,6);
-  let letters7To9 = str.substring(6,9);
-  // If our dictionary doesn't have a list for those first three letters, that means we have no words that start with those letters
-  // If it does have one, check if our full str is included in there as a word or not
-  if (str.length <= 7){
-    if (dictionary.length9[letters1To3] && dictionary.length9[letters1To3].includes(str)){
+  let letters1To2 = str.substring(0,2);
+  let letters3To4 = str.substring(2,4);
+  let letters5To6 = str.substring(4,6);
+  let letters7To8 = str.substring(6,8);
+  
+  if (str.length <= 5){
+    if (dictionary.length5[letters1To2] && dictionary.length5[letters1To2].includes(str)){
       return true;
     }else{
       return false;
     }
   }
-  else if (str.length <= 10){
-    if (dictionary.length12[letters1To3] && dictionary.length12[letters1To3][letters4To6] && dictionary.length12[letters1To3][letters4To6].includes(str)){
+  else if (str.length <= 8){
+    if (dictionary.length8[letters1To2] && dictionary.length8[letters1To2][letters3To4] && dictionary.length8[letters1To2][letters3To4].includes(str)){
+      return true;
+    }else{
+      return false;
+    }
+  }
+  else if (str.length <= 11){
+    if (dictionary.length11[letters1To2] && dictionary.length11[letters1To2][letters3To4] && dictionary.length11[letters1To2][letters3To4][letters5To6] && dictionary.length11[letters1To2][letters3To4][letters5To6].includes(str)){
       return true;
     }else{
       return false;
     }
   }
   else if (str.length <= 16){
-    if (dictionary.length16[letters1To3] && dictionary.length16[letters1To3][letters4To6] && dictionary.length16[letters1To3][letters4To6][letters7To9] && dictionary.length16[letters1To3][letters4To6][letters7To9].includes(str)){
+    if (dictionary.length16[letters1To2] && dictionary.length16[letters1To2][letters3To4] && dictionary.length16[letters1To2][letters3To4][letters5To6] && dictionary.length16[letters1To2][letters3To4][letters5To6][letters7To8] && dictionary.length16[letters1To2][letters3To4][letters5To6][letters7To8].includes(str)){
       return true;
     }else{
       return false;
@@ -88,13 +110,11 @@ function findWordsUtil(board, visited, i, j, str){
 
   if (str.length > 2 && !words.includes(str) && isWord(str)){
     words.push(str);
-  }else if (str.length > 2){
-    // combinationCount++;
   }
 
   for (let row = i-1; row <= i+1 && row <= 3; row++){
     for (let col = j-1; col <= j+1 && col <= 3; col++){
-      if (str.length < 10 && row >= 0 && col >= 0 && !visited[row][col]){
+      if (str.length < 16 && row >= 0 && col >= 0 && !visited[row][col]){
         findWordsUtil(board, visited, row, col, str);
       }
     }
@@ -121,8 +141,8 @@ function findWords(board){
       findWordsUtil(board, visited, i, j, str);
     }
   }
-  // console.log(wordCheckCount + ' dictionary lookups');
-  // console.log(combinationCount + ' letter combinations of 3 letters or more');
+
+  console.log(wordCheckCount + ' dictionary lookups');
   let ms2 = new Date();
   ms2 = ms2.getTime();
   console.log(ms2-ms1 + ' Milliseconds to compute');
