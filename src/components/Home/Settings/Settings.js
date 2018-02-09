901efba81887e2268eed4e1e5a2ada07713f3d24
Loren Pabst <loren.pabst@ALLEGIANCE.LOCAL>
2018-02-09 14:25:10 -0700

    styled settings modal

--- src/components/Home/Settings/Settings.js
+++ src/components/Home/Settings/Settings.js
@@ -21,6 +21,33 @@ class Settings extends Component {
       <section className='settings'>
         
         <p onClick={this.props.close} className="settings_close" >X</p>
+
+        <div className='setting_container'>
+          <p>Organize results</p>
+          <select>
+            <option>alphabetically</option>
+            <option>by length</option>
+          </select>
+        </div>
+
+        <div className='setting_container'>
+          <p>Include words of the following lengths:</p>
+          <div className="checkbox_wrapper"><input type='checkbox' /><p>3</p></div>
+          <div className="checkbox_wrapper"><input type='checkbox' /><p>4</p></div>
+          <div className="checkbox_wrapper"><input type='checkbox' /><p>5</p></div>
+          <div className="checkbox_wrapper"><input type='checkbox' /><p>6</p></div>
+          <div className="checkbox_wrapper"><input type='checkbox' /><p>7</p></div>
+          <div className="checkbox_wrapper"><input type='checkbox' /><p>8</p></div>
+          <div className="checkbox_wrapper"><input type='checkbox' /><p>9</p></div>
+          <div className="checkbox_wrapper"><input type='checkbox' /><p>10</p></div>
+          <div className="checkbox_wrapper"><input type='checkbox' /><p>11</p></div>
+          <div className="checkbox_wrapper"><input type='checkbox' /><p>12</p></div>
+          <div className="checkbox_wrapper"><input type='checkbox' /><p>13</p></div>
+          <div className="checkbox_wrapper"><input type='checkbox' /><p>14</p></div>
+          <div className="checkbox_wrapper"><input type='checkbox' /><p>15</p></div>
+          <div className="checkbox_wrapper"><input type='checkbox' /><p>16</p></div>
+        </div>
+
       </section>
     );
   }
